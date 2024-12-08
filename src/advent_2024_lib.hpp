dd0fe21537cb3d32541c3c583836ca4d0f358cc3
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2024-12-07 18:04:31 -0600

    addressing compilation warnings

--- src/advent_2024_lib.hpp
+++ src/advent_2024_lib.hpp
@@ -3,4 +3,4 @@
 #include <vector>
 #include <string>
 
-std::vector<std::vector<std::string>> read_delim_file(const std::string file, const bool is_whitespace_delim = false, const char delim = ',');
+std::vector<std::vector<std::string>> read_delim_file(const std::string& path, const bool is_whitespace_delim = false, const char delim = ',');
