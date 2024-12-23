3f798f2a01cfbcca47b72874d825be437fdbad02
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2024-12-23 10:08:53 -0500

    adding day 4 part 1 test

---
+++ test/test-day4.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+
+#include <advent_2024_lib.hpp>
+
+TEST(aoc24_day4, pre_processing) {
+    auto data = read_file_lines("data/day4_example.txt");
+    EXPECT_EQ(data.size(), 10);
+}
+
+TEST(aoc24_day4, part1) {
+    auto lines = read_file_lines("data/day4_example.txt");
+    const size_t total_matches = day4_part1(lines);
+    EXPECT_EQ(total_matches, 18);
+}
